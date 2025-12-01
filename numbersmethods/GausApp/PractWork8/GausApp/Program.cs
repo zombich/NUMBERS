49882f9c85dd6a878d6a0420c598ab426d2e98cf
fetma <fetma@ZOMBATKA>
2025-12-01 16:54:14 +0300

    Добавьте файлы проекта.

---
+++ numbersmethods/GausApp/PractWork8/GausApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = 1.5;
+        double b = 2.5;
+        int n = 10;
+        double h = (b - a) / n;
+
+        double sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            double xi = a + i * h;
+
+            double f1 = xi + (h / 2.0) - (h / (2.0 * Math.Sqrt(3.0)));
+            double f2 = xi + (h / 2.0) + (h / (2.0 * Math.Sqrt(3.0)));
+
+            double yi1 = f1 * f1 * Math.Tan(f1 / 2.0);
+            double yi2 = f2 * f2 * Math.Tan(f2 / 2.0);
+
+            sum += yi1 + yi2;
+        }
+
+        double integral = sum * (h / 2.0);
+
+        Console.WriteLine("Интеграл: " + Math.Round(integral, 8));
+        Console.ReadKey();
+    }
+}
+
+
+
+
